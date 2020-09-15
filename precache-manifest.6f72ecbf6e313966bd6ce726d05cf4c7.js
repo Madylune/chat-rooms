d387a13b35baed21b62016a0ab604dda70b42584
Julie Nginn <nginn.julie@gmail.com>
2020-09-15 17:38:22 +0200

    Updates

---
+++ precache-manifest.6f72ecbf6e313966bd6ce726d05cf4c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc2f147d55dd970b79c637b4668ac23",
+    "url": "/chat-rooms/index.html"
+  },
+  {
+    "revision": "3b0bd7b0421b9ea0a525",
+    "url": "/chat-rooms/static/css/main.ecae11e3.chunk.css"
+  },
+  {
+    "revision": "b79253bafdb2f7fd5ee4",
+    "url": "/chat-rooms/static/js/2.8686fa54.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/chat-rooms/static/js/2.8686fa54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0bd7b0421b9ea0a525",
+    "url": "/chat-rooms/static/js/main.da993976.chunk.js"
+  },
+  {
+    "revision": "9a7b1e661b9085d5231d",
+    "url": "/chat-rooms/static/js/runtime-main.598a1fec.js"
+  },
+  {
+    "revision": "ff196929af3161fcb6b94069007c133e",
+    "url": "/chat-rooms/static/media/chatrooms_logo.ff196929.png"
+  },
+  {
+    "revision": "3c77b7c2ebbe3fc92a6ccc296490e98c",
+    "url": "/chat-rooms/static/media/user.3c77b7c2.jpg"
+  }
+]);
